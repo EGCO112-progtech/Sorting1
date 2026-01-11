fd4c691f2d255913606f82f7134733c5c9c9e10f
AjMing <62439174+AjMing@users.noreply.github.com>
2026-01-11 12:57:45 +0700

    Converting argv

--- main.c
+++ main.c
@@ -2,20 +2,20 @@
 //  main.c
 //  First arg
 //
-//  Created by Mingmanas Sivaraksa on 2/1/2567 BE.
+//  Created by Mingmanas Sivaraksa on 2/1/2569 BE.
 //
 
 #include <stdio.h>
 
 int main(int argc, char * argv[]) {
     
-  //  printf("I have %d arguments\n",argc);
-  printf("argc is %d\n",argc);
-    int i,N;
+ printf("I have %d arguments\n",argc);
+ int i,N;
+ int *a;
     for(i=1;i<argc;i++){
         //printf("arg %d: %s\n",i,argv[i]);
          printf("%s\n",argv[i]);
-
+         // Convert argv ---> a?
     
          
     }
